b059e9842efe2ea640c87032ffd86cf6a80bf830
Aaryathapa29 <aaryathapaa@gmail.com>
2026-07-16 22:45:19 +0545

    update project and ignore .env

---
+++ frontend/src/app/components/consultancy/consultancyData.ts
@@ -0,0 +1,37 @@
+export const consultancies = [
+  {
+    id: 1,
+    name: "Consultancy A",
+
+    countries: [
+      "USA",
+      "Canada",
+      "Australia",
+      "United Kingdom",
+      "Japan",
+    ],
+
+    universities: [
+      "University of Toronto",
+      "The University of Sydney",
+    ],
+  },
+
+  {
+    id: 2,
+    name: "Consultancy B",
+
+    countries: [
+      "USA",
+      "Canada",
+      "Australia",
+      "Germany",
+      "New Zealand",
+    ],
+
+    universities: [
+      "University of Melbourne",
+      "Technical University of Munich",
+    ],
+  },
+];
